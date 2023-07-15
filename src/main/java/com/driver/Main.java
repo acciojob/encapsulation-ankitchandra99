212ad6640200674d88fd06c5b80fed3a84677ed7
ANKIT CHANDRA <str.ankit99@gmail.com>
2023-07-15 11:14:22 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+
+
+
+
+   public static void main(String []args){
+       RWOnly r1=new RWOnly();
+       r1.setName("Ankit");
+
+       System.out.println(r1.getName());
+   }
 }
